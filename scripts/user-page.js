d189c41ac482bdc6234dd27056bb50885a876bcb
Lucas Souza <lucas_nevestj@hotmail.com>
2021-04-29 15:38:59 -0300

    Criar script para user-page
    A user-page.html agora tem um script próprio, user-page.js.

---
+++ scripts/user-page.js
@@ -0,0 +1,7 @@
+document.getElementById('study').onClick = function timeToStudy () {
+    location.href="page-videos-estudo.html"
+}
+
+document.getElementById('fun').onClick = function timeToPlay() {
+    location.href="page-videos-lazer.html"
+}
