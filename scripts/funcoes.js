0e7593cdb616cdfb4ca881e70ef61fad2228ecbb
Francisco Florentino de Sousa Filho <ffsf.filho@gmail.com>
2021-04-28 23:15:39 -0300

    Função validaUsuario com redirecionamento de página html

--- scripts/funcoes.js
+++ scripts/funcoes.js
@@ -1,42 +1,25 @@
-var dados = [];
-function buscaUsuarios(){
-    return fetch("https://personal-9ucqet77.outsystemscloud.com/Squad12App/rest/api_usuarios/usuarios")
+const users = "franciscof"
+const password = "fco123"
+
+function validaUsuario(){
+    fetch("https://personal-9ucqet77.outsystemscloud.com/Squad12App/rest/api_usuarios/usuarios", {method: "GET"})
+      .then(response => {return response.json()})
+      .then(data => {
+          console.log(data)
+          console.log(data.length)
+          console.log(users + " == " + password)
+          for(var i =0; i < data.length; i++)
+          {
+            console.log(data[i].Senha + " |" + data[i].Usuario);
+            if(data[i].Usuario == users && data[i].Senha == password){
+              location.href="user-page.html"
+              break
+            } else {
+              location.href="user-cadastro.html"  
+            }
+          }
+        }
+      )
 }
 
-fetch("https://personal-9ucqet77.outsystemscloud.com/Squad12App/rest/api_usuarios/usuarios")
-  .then(
-      response => {return response.json()})
-      .then(
-        data => (
-          //dados.push(data), 
-          data.forEach(function (elemento){
-            //console.log("Dentro do ForEach: " + elemento.Nome)
-            //if(elemento.Nome == "Lucas Souza"){
-              //console.log("Dentro do IF: " + elemento.Nome)
-            //}
-            dados.push(elemento)
-        })
-          //console.log(data)
-          //location.href="user-page.html"
-          //status: response.status
-        )
-      
-      /* .then(res => {
-        //console.log(res.status, res.data[0].Nome)
-        document.write(res.status, 
-          res.data[1].Nome)
-      }) */
-  );
-  
-  console.log("Fora: " + JSON.stringify(dados))
-
-  for(var i = 0; i < 5; i++){
-    console.log(dados[i].Nome)
-  }
-  dados.forEach(function(itens){
-    //console.log(itens.Id + " | " + itens.Nome + " ! " + itens.Senha)
-    console.log("ola ")
-
-  })
-
-
+validaUsuario()
