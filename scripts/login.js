a338b2b78419621560e39d8f289c86481b4ab292
Francisco Florentino de Sousa Filho <ffsf.filho@gmail.com>
2021-04-29 09:35:26 -0300

    Implementado a funcionalidade de Login e criado a página de cadastro como layout espelhado na de login

--- scripts/login.js
+++ scripts/login.js
@@ -2,15 +2,20 @@ function login() {
     let userName = document.getElementById('userAdress');
     let userPassWord = document.getElementById('userPass');
 
-    let nameForUser = 'Squad-12';
-    let passwordForUser = 123450;
-
-    if (userName.value == nameForUser && Number(userPassWord.value) == passwordForUser) {
-        location.href="user-page.html"
-    } else {
-        let wrongData = document.getElementById('messageError')
-        wrongData.style.display = 'inline';
-    }
+    fetch("https://personal-9ucqet77.outsystemscloud.com/Squad12App/rest/api_usuarios/usuarios", {method: "GET"})
+        .then(response => {return response.json()})
+        .then(data => {
+            for(var i =0; i < data.length; i++){
+            if(data[i].Usuario == userName.value && data[i].Senha == userPassWord.value){
+                location.href=`user-page.html?user=${userName.value + "," + data[i].Id}`
+                break
+            } else {
+                let wrongData = document.getElementById('messageError')
+                wrongData.style.display = 'inline';
+            }
+        }
+        }
+    )    
 }
 
 //código para a página de perfil do usuário
